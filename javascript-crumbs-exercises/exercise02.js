3ff5f28eb28cb3a708cd3a8363a43b5a21b6ffad
matthewsaba66 <matthewsaba66@gmail.com>
2014-04-29 11:43:45 +0200

    esercizi javascript

--- javascript-crumbs-exercises/exercise02.js
+++ javascript-crumbs-exercises/exercise02.js
@@ -3,7 +3,7 @@ var s = "";
 for (i = 1; i <= 10; i++){
 	s += "\n";
 	for (j = 1; j <= 10; j++){
-		(j === 10) ? s =s + i*j + "\t" :  s =s + i*j + "," + "\t" ;
+		(j === 10) ? s =s + i*j + "\t" :  s =s + i*j + ",\t" ;
 	}
 }
 console.log(s);
